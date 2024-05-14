ea482edf7477bf6f04e3e8b207be04c44ef23856
Alex da Silva Alves <embaixadorbr@gmail.com>
2024-05-14 20:15:01 -0300

    Conclusao do desafio ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
+
+    public static void main(String[] args) {
+        //TODO:Conhecer e importar a classe Scanner
+        //Exibir as mensagens para o nosso usuario
+        //Obter pelo Scanner os valores digitados no terminal
+        //Exibir a mensagem conta criada
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        
+        System.out.println("Digite o numero da conta ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite a Agencia ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o valor depositado ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque!");
+
+    }
 }
